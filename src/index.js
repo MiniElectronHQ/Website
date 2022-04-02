f20a5d6e04d0e5c781fc962863279621b22d3fb6
Wynter (beepop) <wynter@monetizedesign.com>
2022-04-02 02:09:30 -0400

    website in react basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import "./assets/stylesheets/index.css";
+
+import App from "./views/App";
+import Codeman from "./views/Codeman";
+
+const container = document.querySelector("#container");
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <Helmet>
+      <title>miniElection - small open source applications</title>
+      <meta
+        name="description"
+        content="Small apps written to do a variety of cool coder related things."
+      />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="codeman" element={<Codeman />} />
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/views/App.js
@@ -0,0 +1,32 @@
+import logo from "../assets/images/logo.png";
+
+function App() {
+  return (
+    <div className="bg-slate-100 text-slate-700 h-screen flex items-center">
+      <section className="mx-auto text-center">
+        <img src={logo} alt="code.man" className="block mx-auto mb-4 w-8" />
+        <h1 className="text-3xl font-extrabold">
+          <span className="text-slate-500">mini</span>Electron
+        </h1>
+        <p>Small open source applications.</p>
+
+        <main className="grid grid-cols-3 gap-3 mt-12 text-white">
+          <div className="border border-gray-900 bg-slate-900 p-6 shadow-xl rounded">
+            <h2 className="text-xl font-medium">
+              code<span className="text-brand-color">.</span>man
+            </h2>
+            <p className="text-xs text-slate-400">[folder viewer for coders]</p>
+            <a
+              href="/codeman"
+              className="py-1 px-2 border border-slate-900 rounded mt-2 font-medium inline-block bg-slate-700 text-sm"
+            >
+              Open
+            </a>
+          </div>
+        </main>
+      </section>
+    </div>
+  );
+}
+
+export default App;
